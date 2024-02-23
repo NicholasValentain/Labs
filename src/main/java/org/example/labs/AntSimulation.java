1d266cff7c418daebe0b559a70685c2ab421a2b1
NicholasValentain <87443329+NicholasValentain@users.noreply.github.com>
2024-02-23 20:31:16 +0700

    Timer created

--- src/main/java/org/example/labs/AntSimulation.java
+++ src/main/java/org/example/labs/AntSimulation.java
@@ -1,8 +1,10 @@
 package org.example.labs;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.StackPane;
@@ -15,6 +17,7 @@ import javafx.stage.Stage;
 import javafx.scene.control.Label;
 
 public class AntSimulation extends Application {
+    private long simulationStartTime; // Время начала симуляции
     public static void main(String[] args) {
         launch(args);
     }
@@ -37,11 +40,59 @@ public class AntSimulation extends Application {
 
 
         Scene scene = new Scene(root, 1200, 900);
+
+        Rectangle rectangle = new Rectangle();
+        Text times = new Text("Тime: ");
+
         scene.setOnKeyPressed(event -> {
             if (event.getCode() == KeyCode.B) {
+                simulationStartTime = System.currentTimeMillis();
                 habitat.startSimulation();
-            } else if (event.getCode() == KeyCode.E) {
+            }
+            else if (event.getCode() == KeyCode.E) {
                 habitat.stopSimulation();
+            }
+            else if (event.getCode() == KeyCode.T) {
+                boolean isRectangleShown = root.getChildren().contains(rectangle);
+
+                if (!isRectangleShown) {
+                    StackPane.setAlignment(rectangle, Pos.TOP_LEFT);
+                    rectangle.setTranslateX(1000); // Устанавливаем координату X
+                    rectangle.setTranslateY(0); // Устанавливаем координату Y
+                    rectangle.setWidth(200); // Устанавливаем ширину
+                    rectangle.setHeight(50); // Устанавливаем высоту
+                    rectangle.setFill(Color.WHITE);// Устанавливаем цвет заливки прямоугольника
+                    root.getChildren().add(rectangle);// Добавляем прямоугольник на сцену или другой контейнер
+
+                    long simulationEndTime = System.currentTimeMillis();
+                    long simulationTime = (simulationEndTime - simulationStartTime) / 1000;
+
+
+                    StackPane.setAlignment(times, Pos.TOP_LEFT);
+                    times.setFont(Font.font("Arial Rounded MT", 20)); // устанавливаем шрифт Arial Rounded MT размером 35
+                    times.setFill(Color.BLACK); // устанавливаем цвет текста на наш свой собственный цвет
+                    times.setTranslateX(1005); // Устанавливаем координату X
+                    times.setTranslateY(10); // Устанавливаем координату Y
+                    root.getChildren().add(times);
+                    // Определяем таймер для обновления времени
+                    AnimationTimer timer = new AnimationTimer() {
+                        @Override
+                        public void handle(long now) {
+                            long currentTime = System.currentTimeMillis();
+                            long simulationTime = (currentTime - simulationStartTime) / 1000;
+                            times.setText("Time: " + simulationTime); // Обновляем текст с текущим временем
+                        }
+                    };
+                    // Начинаем таймер
+                    timer.start();
+                }
+                else {
+                    // Если прямоугольник уже отображен, скрываем его
+                    root.getChildren().remove(rectangle);
+                    // Удаляем текст
+                    root.getChildren().remove(times);
+                }
+
             }
             root.getChildren().remove(descriptionLabel);
         });
