8deb54c5699ae971c5a9a39d9e64172ca2c34b08
NickolasValentine <kuznetsovvs@mer.ci.nsu.ru>
2024-03-25 22:59:56 +0700

    v2.1.7

--- src/main/java/org/example/labs/controllres/controller.java
+++ src/main/java/org/example/labs/controllres/controller.java
@@ -6,11 +6,15 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import org.example.labs.model.Habitat; // Импортируем класс Habitat
 import org.example.labs.main.AntSimulation;
 
+import java.util.Locale;
+import java.util.Map;
+import java.util.TreeMap;
 
 
 public class controller {
@@ -21,9 +25,12 @@ public class controller {
     @FXML
     public CheckBox cbShowInfo;
     @FXML
-    public TextField N1, N2;
+    public TextField N1, N2, L1, L2;
     @FXML
     public ComboBox P1, P2;
+    @FXML
+    public TableView<Map.Entry<Integer, Long>> tableView;
+    public TreeMap<Integer, Long> treeMap;
     private Habitat habitat; // Добавляем поле для хранения ссылки на habitat
     private AntSimulation antSimulation;
 
@@ -37,14 +44,18 @@ public class controller {
         this.antSimulation = antSimulation;
         N1.setText("1");
         N2.setText("1");
+        L1.setText("1");
+        L2.setText("1");
         ShowTime.setSelected(true);
         btnStop.setDisable(true);
         // Установка списка элементов в ComboBox
         P1.setItems(options);
         P2.setItems(options);
-        P1.setValue("10");
+        P1.setValue("100");
         P2.setValue("100");
 
+
+        tableView.setVisible(false);
     }
 
     @FXML
@@ -58,6 +69,8 @@ public class controller {
 
                     N1.setDisable(true);
                     N2.setDisable(true);
+                    L1.setDisable(true);
+                    L2.setDisable(true);
                     P1.setDisable(true);
                     P2.setDisable(true);
                     cbShowInfo.setDisable(true);
@@ -73,6 +86,8 @@ public class controller {
 
                     N1.setDisable(false);
                     N2.setDisable(false);
+                    L1.setDisable(false);
+                    L2.setDisable(false);
                     P1.setDisable(false);
                     P2.setDisable(false);
                     cbShowInfo.setDisable(false);
@@ -86,12 +101,12 @@ public class controller {
         if (ShowTime.isSelected() && !antSimulation.timerVisible) {
             antSimulation.timerVisible = true;
             antSimulation.root.getChildren().add(antSimulation.times);
-            HideTime.setSelected(false);
+            //HideTime.setSelected(false);
         }
         else if (HideTime.isSelected() && antSimulation.timerVisible) {
             antSimulation.timerVisible = false;
             antSimulation.root.getChildren().remove(antSimulation.times);
-            ShowTime.setSelected(false);
+            //ShowTime.setSelected(false);
         }
     }
 
@@ -102,14 +117,21 @@ public class controller {
     public boolean checkError() {
         int n1 = 1;
         int n2 = 1;
+        int l1 = 1;
+        int l2 = 1;
         try {
             n1 = Integer.parseInt(N1.getText());
             n2 = Integer.parseInt(N2.getText());
+            l1 = Integer.parseInt(L1.getText());
+            l2 = Integer.parseInt(L2.getText());
 
-            if (n1 < 1 || n2 < 1) throw new NumberFormatException("Число меньше 1");
+            if (n1 < 1 || n2 < 1 || l1 < 1 || l2 < 1) throw new NumberFormatException("Число меньше 1");
+
 
             habitat.N1 = n1;
             habitat.N2 = n2;
+            habitat.L1 = l1;
+            habitat.L2 = l2;
 
             habitat.P1 = Double.parseDouble((String) P1.getValue()) / 100;
             habitat.P2 = Double.parseDouble((String) P2.getValue()) / 100;
@@ -124,6 +146,8 @@ public class controller {
             // Проверяем, на некорректный ввод
             if (!N1.getText().matches("\\d+") || n1 < 1) N1.setText("1");
             if (!N2.getText().matches("\\d+") || n2 < 1) N2.setText("1");
+            if (!L1.getText().matches("\\d+") || l1 < 1) L1.setText("1");
+            if (!L2.getText().matches("\\d+") || l2 < 1) L2.setText("1");
 
             // Get the Stage.
             Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
@@ -148,6 +172,8 @@ public class controller {
 
                     N1.setDisable(true);
                     N2.setDisable(true);
+                    L1.setDisable(true);
+                    L2.setDisable(true);
                     P1.setDisable(true);
                     P2.setDisable(true);
                     cbShowInfo.setDisable(true);
@@ -164,6 +190,8 @@ public class controller {
 
                     N1.setDisable(false);
                     N2.setDisable(false);
+                    L1.setDisable(false);
+                    L2.setDisable(false);
                     P1.setDisable(false);
                     P2.setDisable(false);
                     cbShowInfo.setDisable(false);
@@ -215,4 +243,74 @@ public class controller {
             }
         }
     }
+
+
+    @FXML
+    private void showTableView() {
+
+        // Очистить таблицу перед добавлением новых данных
+        tableView.getItems().clear();
+        tableView.getColumns().clear();
+        treeMap = habitat.spawnTimes;
+
+        //tableView = new TableView<Map.Entry<Integer, Long>>();
+
+        //ObservableList<Map.Entry<Integer, Long>> teamMembers = FXCollections.observableArrayList(treeMap.entrySet());
+
+
+//        TableColumn<Map.Entry<Integer, Long>, Integer> IDColumn = (TableColumn<Map.Entry<Integer, Long>, Integer>) tableView.getColumns().get(0);
+//        IDColumn.setCellValueFactory(new PropertyValueFactory<Map.Entry<Integer, Long>, Integer>("key"));
+//        tableView.getColumns().add(IDColumn);
+//
+//        TableColumn<Map.Entry<Integer, Long>, Long> valueColumn = (TableColumn<Map.Entry<Integer, Long>, Long>) tableView.getColumns().get(1);
+//        valueColumn.setCellValueFactory(new PropertyValueFactory<Map.Entry<Integer, Long>, Long>("value"));
+//        tableView.getColumns().add(valueColumn);
+
+
+
+//        tableView.setItems(teamMembers);
+//
+//        TableColumn<Map.Entry<Integer, Long>, Integer> IDColumn = new TableColumn<Map.Entry<Integer, Long>, Integer>("key");
+//        IDColumn.setCellValueFactory(new PropertyValueFactory<Map.Entry<Integer, Long>, Integer>("key"));
+//        tableView.getColumns().add(IDColumn);
+//
+//        TableColumn<Map.Entry<Integer, Long>, Long> valueColumn = new TableColumn<Map.Entry<Integer, Long>, Long>("value");
+//        valueColumn.setCellValueFactory(new PropertyValueFactory<Map.Entry<Integer, Long>, Long>("value"));
+//        tableView.getColumns().add(valueColumn);
+
+
+        // Добавляем данные в колонки таблицы
+        TableColumn<Map.Entry<Integer, Long>, Integer> keyColumn = new TableColumn<>("Key");
+        keyColumn.setCellValueFactory(cellData -> {
+            Integer key = cellData.getValue().getKey();
+            return new javafx.beans.value.ObservableValueBase<Integer>() {
+                @Override
+                public Integer getValue() {
+                    return key;
+                }
+            };
+        });
+
+        TableColumn<Map.Entry<Integer, Long>, Long> valueColumn = new TableColumn<>("Value");
+        valueColumn.setCellValueFactory(cellData -> {
+            Long value = cellData.getValue().getValue();
+            return new javafx.beans.value.ObservableValueBase<Long>() {
+                @Override
+                public Long getValue() {
+                    return value;
+                }
+            };
+        });
+
+        tableView.getColumns().addAll(keyColumn, valueColumn);
+
+        // Добавляем данные в таблицу
+        for (Map.Entry<Integer, Long> entry : treeMap.entrySet()) {
+            tableView.getItems().add(entry);
+        }
+
+        // Показываем TableView после создания
+        tableView.setVisible(true);
+    }
+
 }
